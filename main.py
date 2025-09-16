7647c82b38106b85f323d5b6b6c59278b47f1cfd
sanpixel <sanjay149@gmail.com>
2025-09-16 13:27:16 -0400

    Test push behavior

--- main.py
+++ main.py
@@ -1658,7 +1658,7 @@ def main():
             tab1, tab2 = st.tabs(["📂 Local Files", "☁️ Google Drive"])
         
         # Only access tabs when they're defined for the authorized user
-        # Prevents UnboundLocalError with tabs
+        # Prevents UnboundLocalError when accessing tabs
         if user and user.get('email') == 'sanjay149@gmail.com':
             with tab1:
             if pdf_files:
